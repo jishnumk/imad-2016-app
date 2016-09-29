b5620f5c9c0db4f557d9cc88496256bc0998debb
Jishnu Muralikrishna <jishnumk90@gmail.com>
2016-09-29 16:07:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,6 +6,65 @@ var app = express();
 app.use(morgan('combined'));
 
 
+var articleOne={
+    title:'Article One',
+    heading:'Article One'
+    date:'Sept 5, 2016'
+    content:` <p>
+                    This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.
+                </p>
+                
+                <p>
+                    This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.
+                </p>
+                
+                <p>
+                    This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.This content for my first article.
+                </p>`
+};
+
+function createTemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    var htmlTemplate=`<html>
+                        <head>
+                        
+                            <title>
+                                ${title}
+                            </title>
+                            <meta name="viewport" content="width-device-width, intial-scale-1">
+                            <link href="/ui/style.css" rel="stylesheet" />
+                            
+                        </head>
+                        
+                        <body>
+                            <div class="container">
+                                <div>
+                                    <a href="/">Home</a>
+                                </div>
+                                <hr/>
+                                
+                                <h3>${heading}</h3>
+                                
+                                <div>
+                                    ${date}
+                                </div>
+                                
+                                <div>
+                                    ${content}
+                                    
+                                </div>
+                            </div>
+                            
+                        </body>
+                    
+                        
+                    </html>
+    `;
+    return htmltemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));

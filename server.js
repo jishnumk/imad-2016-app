eddcc2e79b2a331103f4554fd46f06f2a26f4ef2
Jishnu Muralikrishna <jishnumk90@gmail.com>
2016-11-26 15:38:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/logo.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'logo.png'));
 });
 
 app.get('/ui/bg.jpg', function (req, res) {

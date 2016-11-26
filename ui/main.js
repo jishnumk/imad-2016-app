3cfdb7196991f4090f2bc6fee30429734e485a2d
Jishnu Muralikrishna <jishnumk90@gmail.com>
2016-11-26 17:41:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,6 @@
-$("#leftbtn").click(function(){$("#slider").toggleClass("in");});
+$('#leftbtn').click(function(){
+    $('#slider').addClass('in');
+});
+
 $(".closebutton").click(function(){$(this).parent().removeClass("in");});
 
